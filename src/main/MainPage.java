701472e9868428773f1334d893a4eba7ae6c568f
yhj <tntj123@naver.com>
2021-08-30 18:19:05 +0900

    메인 페이지 생성

---
+++ src/main/MainPage.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.util.Scanner;
+
+public class MainPage {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String name, stNum;
+		int num;
+		System.out.println("--학생 관리 프로그램 시작--");
+		while (true) {
+			System.out.println("1.수정 2.전체보기 3.하나만 보기 4.수정 5.삭제");
+			num = scan.nextInt();
+			switch (num) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			}
+		}
+	}
+}
