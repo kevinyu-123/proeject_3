e40d7384e0c940f5a8ae00ec2a8945d33d288e93
yhj <tntj123@naver.com>
2021-08-30 18:29:06 +0900

    수정 본

--- src/main/MainPage.java
+++ src/main/MainPage.java
@@ -9,7 +9,7 @@ public class MainPage {
 		int num;
 		System.out.println("--학생 관리 프로그램 시작--");
 		while (true) {
-			System.out.println("1.수정 2.전체보기 3.하나만 보기 4.수정 5.삭제");
+			System.out.println("1.생성 2.전체보기 3.하나만 보기 4.수정 5.삭제");
 			num = scan.nextInt();
 			switch (num) {
 			case 1:
