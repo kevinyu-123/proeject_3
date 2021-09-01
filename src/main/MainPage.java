c49e769f1c6a47469168f80156760af07685710f
kimjinsu <soosos11234@gmail.com>
2021-09-01 15:35:32 +0900

    삭제 작업중

--- src/main/MainPage.java
+++ src/main/MainPage.java
@@ -6,7 +6,9 @@ import java.util.Scanner;
 public class MainPage {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		String name = null, stNum;
+		ArrayList<STU> al = new ArrayList<STU>();
+		String name = null, stuNo;
+		int phoneNo;
 		int num;
 		System.out.println("--학생 관리 프로그램 시작--");
 		while (true) {
@@ -14,10 +16,43 @@ public class MainPage {
 			num = scan.nextInt();
 			switch (num) {
 			case 1: 
+				System.out.println("학번 입력 ");
+			stuNo = scan.next();
+			System.out.println("이름 입력 ");
+			name = scan.next();
+			System.out.println("전화번호 입력 ");
+			phoneNo = scan.nextInt();		
 			
-			case 2:
+			STU s = new STU();
+			s.setStuNo(stuNo);
+			s.setName(name);
+			s.setPhoneNo(phoneNo);
+			
+			al.add(s);
+				break;
+			case 2:System.out.println("----학생 전체 보기----");
+			for(int i =0; i<al.size();i++) {
+				System.out.println("학생 이름: "+al.get(i).getName());
+				System.out.println("학생 학번: "+al.get(i).getStuNo());
+				System.out.println("학생 번호: "+al.get(i).getPhoneNo());
+				System.out.println("---------------");}
 				break;
 			case 3:
+				System.out.println("=== 검색 할 학번을 입력해주세요 ===");
+			stuNo = scan.next();
+
+			for (int i = 0; i < al.size(); i++) {
+				STU stu02 = al.get(i);
+				if (stuNo.equals(stu02.getStuNo())) {
+					System.out.println("학생 이름 : " + al.get(i).getStuNo());
+					System.out.println("학생 학번 : " + al.get(i).getName());
+					System.out.println("학생 번호 : " + al.get(i).getPhoneNo());
+				}
+				if (stuNo.contains(stuNo) != true) {
+					System.out.println("존재하지 않는 학번입니다.");
+				}
+			}
+
 				break;
 			case 4:
 				break;
@@ -26,13 +61,12 @@ public class MainPage {
 					while(true) {
 						System.out.println("삭제하실 회원이름을 입력해주세요.");
 						String delName = scan.next();
-						
-						if(delName == name) {
-						ArrayList<DelN> arr = new ArrayList<DelN>();
-						arr.remove(name);
+						if(delName.contains(name)) {
+						al.remove(delName);
 						System.out.println(delName+"의 정보가 삭제되었습니다.");
 						} else {
 							System.out.println("존재하지 않는 회원입니다.");
+							break;
 						}break;
 					}
 			}
